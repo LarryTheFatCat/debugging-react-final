87ee4f400f94c9b894f906ec9fac948a580f2bbc
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-28 17:01:46 -0500

    space out list, add hamburger icon clcik function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
 import '../components/Navbar.css'
 import NavBarLogo from '../assets/LogoMaker-removebg-preview.svg'
+import { Burger } from '@mantine/core';
+import { useState } from 'react';
 
 const RefreshIcon = () => {
     window.location.reload(false);
 }
 
 export const Navbar = () => {
+    const [opened, setOpened] = useState(false);
+    const title = opened ? 'Close navigation' : 'Open navigation';
+
+
+    const OpenFunction = () => {
+        setOpened(!opened);
+    }
+    
     return (
         <div className="navContainer" id="navbar-main">
             <nav className="nav nav-main">
+                <Burger color='white' opened={opened} onClick={OpenFunction} title={title} />
+                <img className="nav nav-image nav-list" src={NavBarLogo} alt="navigationbar icon display" onClick={RefreshIcon} />
                 <ul className="nav-list">
-                    <img className="nav nav-image" src={NavBarLogo} alt="navigationbar icon display" onClick={RefreshIcon} />
-                    <li className="nav nav-home" onClick={RefreshIcon}> Home </li>
-                    <li className="nav nav-about" onClick={RefreshIcon}>About Me</li>
-                    <li className="nav nav-contacts" onClick={RefreshIcon}>Contacts</li>
+                    <li className="nav nav-home nav-list" onClick={RefreshIcon}>
+                        Contacts
+                    </li>
+                    <li className="nav nav-about nav-list" onClick={RefreshIcon}>
+                        About Me
+                    </li>
+                    <li className="nav nav-contacts" onClick={RefreshIcon}>
+                        Home
+                    </li>
                 </ul>
             </nav>
         </div>
