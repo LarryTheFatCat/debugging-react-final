ed5898dc2b594ca25d2394589f422543a01a001d
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-28 02:33:40 -0500

    create navbar before responsive component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import '../components/Navbar.css'
+import navBarLogo from '../assets/LogoMaker-removebg-preview.svg'
+
+
+
+const RefreshIcon = () => {
+    window.location.reload(false);
+}
+
+export const Navbar = () => {
+    return (
+        <div className="navContainer" id="navbar-main">
+            <nav className="nav nav-main">
+                <ul className="nav-list">
+                    <img className="nav nav-image" src={navBarLogo} alt="" onClick={RefreshIcon} />
+                    <li className="nav nav-home"> Home </li>
+                    <li className="nav nav-about">About Me</li>
+                    <li className="nav nav-contacts">Contacts</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+
+export default Navbar;
