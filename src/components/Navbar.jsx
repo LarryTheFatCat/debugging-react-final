deaa78d4ca7536c432f7d6683973e5d02143f316
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-28 02:50:25 -0500

    forgot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import '../components/Navbar.css'
-import navBarLogo from '../assets/LogoMaker-removebg-preview.svg'
-
-
+import NavBarLogo from '../assets/LogoMaker-removebg-preview.svg'
 
 const RefreshIcon = () => {
     window.location.reload(false);
@@ -12,7 +10,7 @@ export const Navbar = () => {
         <div className="navContainer" id="navbar-main">
             <nav className="nav nav-main">
                 <ul className="nav-list">
-                    <img className="nav nav-image" src={navBarLogo} alt="" onClick={RefreshIcon} />
+                    <img className="nav nav-image" src={NavBarLogo} alt="" onClick={RefreshIcon} />
                     <li className="nav nav-home"> Home </li>
                     <li className="nav nav-about">About Me</li>
                     <li className="nav nav-contacts">Contacts</li>
